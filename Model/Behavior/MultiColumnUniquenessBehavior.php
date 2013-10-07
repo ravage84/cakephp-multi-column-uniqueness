d9a6129504ca253404e5797c2f3a6f4b4e297d21
Marc Würth <ravage@bluewin.ch>
2013-10-07 23:59:25 +0200

    Fixed a bug, improved CS and DocBlocks in MultiColumnUniquenessBehavior

--- Model/Behavior/MultiColumnUniquenessBehavior.php
+++ Model/Behavior/MultiColumnUniquenessBehavior.php
@@ -2,10 +2,10 @@
 /**
  * PHP 5
  *
- * @copyright Marc Würth @ ORCA Services AG
+ * @copyright 2013 Marc Würth @ ORCA Services AG
  * @author Marc Würth
  * @version 0.1
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
  **/
 
 App::uses('ModelBehavior', 'Model');
@@ -13,22 +13,96 @@ App::uses('ModelBehavior', 'Model');
 /**
  * MultiColumnUniqueness Behavior
  *
- * TODO Write long description
+ * Uses the CakePHP built-in method Model::isUnique() to check multiple columns for their uniqueness.<br>
+ * Can be configured in many ways.
  *
- * @link http://book.cakephp.org/2.0/en/models/saving-your-data.html#using-created-and-modified
+ * **One field group set, without error message set (most simple and shortest way):**
+ *
+ * The validation rule will be executed for field1 only.<br>
+ * The missing error message will be generated automatically.
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array('field1', 'field2')
+ * ));
+ * </code>
+ *
+ * **One field group set, without error message set (array in array):**
+ *
+ * Same as above.
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array(array('field1', 'field2'))
+ * ));
+ * </code>
+ *
+ * **One field group and its error message set:**
+ *
+ * The validation rule will be executed for field1 only.
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array('field1', 'field2'),
+ * 	'errMsg' => 'Field1 and field2 must be unique.',
+ * ));
+ * </code>
+ *
+ * **Two field groups and their corresponding error message set:**
+ *
+ * The validation rule for the first field group will be executed for field1 only.<br>
+ * The validation rule for the second field group will be executed for field3 only.
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array(array('field1', 'field2'), array('field3', 'field4')),
+ * 	'errMsg' => array('Field1 and field2 must be unique.', 'Field3 and field4 must be unique.'),
+ * ));
+ * </code>
+ *
+ * **Two field groups set but only the error message of the first group set:**
+ *
+ * The validation rule for the first field group will be executed for field1 only.<br>
+ * The validation rule for the second field group will be executed for field3 only.<br>
+ * The missing error message will be generated automatically.
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array(array('field1', 'field2'), array('field3', 'field4')),
+ * 	'errMsg' => array('Field1 and field2 must be unique.', 'Field3 and field4 must be unique.'),
+ * ));
+ * </code>
+ *
+ * **Option onlyOnce disabled:**
+ *
+ * The validation rule will be executed for all fields of **each** group!
+ *
+ * <code>
+ * public $actsAs = array('MultiColumnUniqueness.MultiColumnUniqueness' => array(
+ * 	'fields' => array(array('field1', 'field2'), array('field3', 'field4')),
+ * 	'errMsg' => array('Field1 and field2 must be unique.', 'Field3 and field4 must be unique.'),
+ * 	'onlyOnce' => false,
+ * ));
+ * </code>
+ *
+ * @link http://book.cakephp.org/2.0/en/models/data-validation.html#Model::Validation::isUnique
+ * @see Model::isUnique() Makes use of
  */
 class MultiColumnUniquenessBehavior extends ModelBehavior {
 
 /**
  * Default settings for a model that has this behavior attached
  *
+ * No fields must be unique<br>
+ * No field group, no message<br>
+ * Add the validation rule to the first column found in $this->data only
+ *
  * @var array
- * @link http://book.cakephp.org/2.0/en/models/saving-your-data.html#using-created-and-modified
  */
 	protected $_defaults = array(
-		'fields' => array(), // No fields must be unique
-		'onlyOnce' => true, // Add the validation rule to the first column found in $this->data only
-		'errMsg' => array() // No field group, no message
+		'fields' => array(),
+		'onlyOnce' => true,
+		'errMsg' => array()
 	);
 
 /**
@@ -38,6 +112,9 @@ class MultiColumnUniquenessBehavior extends ModelBehavior {
  * merges them with the the defaults.
  * If the fields array is one dimensional,
  * it moves it to the second dimension.
+ * If the errMsg array is one dimensional,
+ * it moves it to the second dimension.
+ * Adds missing error messages for each field group.
  *
  * @param Model $model Model using this behavior
  * @param array $config Configuration settings for $model
@@ -53,57 +130,61 @@ class MultiColumnUniquenessBehavior extends ModelBehavior {
 		if (!empty($this->settings[$model->alias]['fields'][0]) &&
 			!is_array($this->settings[$model->alias]['fields'][0])) {
 			$fieldGroup = $this->settings[$model->alias]['fields'];
-			unset($this->settings[$model->alias]['fields']);
+			$this->settings[$model->alias]['fields'] = array();
 			$this->settings[$model->alias]['fields'] = array($fieldGroup);
 		}
 		if (!empty($this->settings[$model->alias]['errMsg']) &&
 			!is_array($this->settings[$model->alias]['errMsg'])) {
 			$errMsg = $this->settings[$model->alias]['errMsg'];
-			unset($this->settings[$model->alias]['errMsg']);
+			$this->settings[$model->alias]['errMsg'] = array();
 			$this->settings[$model->alias]['errMsg'] = array($errMsg);
 		}
 
-		for ($groupNr = 0;$groupNr < count($this->settings[$model->alias]['fields']); $groupNr++) {
+		$fieldGroupCount = count($this->settings[$model->alias]['fields']);
+		for ($groupNr = 0;$groupNr < $fieldGroupCount; $groupNr++) {
 			if (!isset($this->settings[$model->alias]['errMsg'][$groupNr])) {
 				$fieldNames = String::toList($this->settings[$model->alias]['fields'][$groupNr]);
 				$this->settings[$model->alias]['errMsg'][$groupNr] = sprintf('The fields %s must be unique.', $fieldNames);
 			}
 		}
-		FireCake::log($this->settings);
 	}
 
 /**
- * Add the multiColumnUnique data validation rule dynamically
+ * Adds the multiColumnUnique data validation rule dynamically
  *
- * Loops through the field groups and its fields which need to be unique.
- * First removes the 'multiColumnUniqueness' rule from all unique fields and then
- * adds the rule to the first of the related fields found in the data array only.
+ * Loops through the field groups and their fields which need to be unique.
+ * First removes the multiColumnUniqueness rule from each unique field.
+ * If the 'onlyOnce' option is set, it adds the rule to the first of the
+ * relevant fields found in the data array only.
+ * Otherwise it adds the rule to each field of the field group.
  *
- * @param Model $Model Model using this behavior
- * @param array $options Unused
- * @return bool
- * @todo Implement the 'onlyOnce' option
+ * @param Model $model Model using this behavior
+ * @param array $options Options passed from Model::save() (unused).
+ * @return boolean True if validate operation should continue, false to abort
  */
-	public function beforeValidate(Model $Model, $options = array()) {
-		for ($groupNr = 0;$groupNr < count($this->settings[$Model->alias]['fields']); $groupNr++) {
-			$uniqueFieldGrp = $this->settings[$Model->alias]['fields'][$groupNr];
+	public function beforeValidate(Model $model, $options = array()) {
+		$fieldGroupCount = count($this->settings[$model->alias]['fields']);
+		for ($groupNr = 0;$groupNr < $fieldGroupCount; $groupNr++) {
+			$uniqueFieldGrp = $this->settings[$model->alias]['fields'][$groupNr];
 			foreach ($uniqueFieldGrp as $uniqueField) {
-				$Model->validator()->remove($uniqueField, 'multiColumnUniqueness');
+				if ($model->validator()->getField($uniqueField)) {
+					$model->validator()->remove($uniqueField, 'multiColumnUniqueness');
+				}
 			}
 			foreach ($uniqueFieldGrp as $uniqueField) {
-				if (isset($Model->data[$Model->name]) && array_key_exists($uniqueField, $Model->data[$Model->name])) {
-					$Model->validator()->add($uniqueField, 'multiColumnUniqueness', array(
+				if (isset($model->data[$model->name]) && array_key_exists($uniqueField, $model->data[$model->name])) {
+					$model->validator()->add($uniqueField, 'multiColumnUniqueness', array(
 						'rule' => array('multiColumnUniqueness', $uniqueFieldGrp),
-						'message' => $this->settings[$Model->alias]['errMsg'][$groupNr],
+						'message' => $this->settings[$model->alias]['errMsg'][$groupNr],
 					));
-					if ($this->settings[$Model->alias]['onlyOnce']) {
+					if ($this->settings[$model->alias]['onlyOnce']) {
 						break;
 					}
 				}
 			}
 		}
 
-		return parent::beforeValidate($Model, $options);
+		return parent::beforeValidate($model, $options);
 	}
 
 /**
@@ -113,27 +194,29 @@ class MultiColumnUniquenessBehavior extends ModelBehavior {
  * and the validation is for an existing record, the field will be loaded.
  * Otherwise it's considered to be empty (e.g. when adding).
  *
- * @param Model $Model Model using this behavior
+ * @param Model $model Model using this behavior
  * @param $data array Unused
  * @param $fields array The fields to be checked
  * @return bool True if valid, else false
+ * @see Model::isUnique() Makes use of
  */
-	function multiColumnUniqueness(Model $Model, $data, $fields) {
+	public function multiColumnUniqueness(Model $model, $data, $fields) {
 		if (!is_array($fields)) {
 			$fields = array($fields);
 		}
 		$check = array();
-		foreach($fields as $key) {
-			if (isset($Model->data[$Model->name][$key])) {
-				$value = $Model->data[$Model->name][$key];
-			} elseif (!empty($Model->id)) {
-				$value = $Model->field($key, array($Model->primaryKey => $Model->data[$Model->name][$Model->primaryKey]));
+		foreach ($fields as $key) {
+			if (isset($model->data[$model->name][$key])) {
+				$value = $model->data[$model->name][$key];
+			} elseif (!empty($model->id)) {
+				$value = $model->field($key, array($model->primaryKey => $model->data[$model->name][$model->primaryKey]));
 			} else {
-				$value = '';
+				$value = null;
 			}
 			$check[$key] = $value;
 		}
-		return $Model->isUnique($check, false);
+
+		return $model->isUnique($check, false);
 	}
 
 }
